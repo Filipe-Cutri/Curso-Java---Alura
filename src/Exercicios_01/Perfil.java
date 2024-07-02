002b5cc263dc3811b933ebc4b58bac5ce0bfbf3e
[Filipe Cutri] <filipe.cutri@alunos.fho.edu.br>
2024-07-01 22:22:20 -0300

    Primeiros códigos em Java

---
+++ src/Exercicios_01/Perfil.java
@@ -0,0 +1,8 @@
+package Exercicios_01;
+
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Filipe");
+        System.out.println("Tudo bem ?");
+    }
+}
