87f3f0303497268423ee6b453753ec36ae91f493
[Filipe Cutri] <filipe.cutri@alunos.fho.edu.br>
2024-07-01 22:46:44 -0300

    Desafio Java - Conta bancária

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
-        System.out.println("Filme: Top Gun: Maverick");
+        Scanner scanner = new Scanner(System.in);
+        String nomeCliente = "Filipe Alberto Cutri";
+        String tipoConta = "Conta corrente";
+        double saldoInicial = 2500;
+        double valorRecebido;
+        double valorTransferido;
+        int operacaoes = 0;
 
-        int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 9.5;
+        System.out.println("-----------------------------------");
+        System.out.println("Seja bem vindo ao banco FACBank");
+        System.out.println("-----------------------------------");
 
-        double media = (9.8 + 7.0 + 8.0) /3;
-        System.out.println(media);
-        String sinopse = "Filme de aventura, com Tom Cruise como protagonista";
-        System.out.println(sinopse);
+        System.out.println("Dados iniciais do cliente:");
+        System.out.println("\nNome: " + nomeCliente);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.printf("Saldo incial: R$ %.2f%n", saldoInicial);
+        System.out.println("\n-----------------------------------");
 
-        int classificacao;
-        classificacao = (int) (media/2);
-        System.out.println(classificacao);
+        while (operacaoes != 4) {
+            System.out.println("Operações:");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Receber valor");
+            System.out.println("3 - Transferir valor");
+            System.out.println("4 - Sair");
+            System.out.println("Digite a opção desejada: ");
+            operacaoes = scanner.nextInt();
+
+            switch (operacaoes) {
+                case 1:
+                    System.out.printf("Você possui um saldo de R$ %.2f%n", saldoInicial);
+                    break;
+                case 2:
+                    System.out.println("Digite o valor recebido: ");
+                    valorRecebido = scanner.nextDouble();
+
+                    saldoInicial += valorRecebido;
+                    System.out.printf("O seu saldo atualizado é de R$ %.2f%n", saldoInicial);
+                    break;
+                case 3:
+                    System.out.println("Digite o valor que deseja transferir: ");
+                    valorTransferido = scanner.nextDouble();
+
+                    if (valorTransferido > saldoInicial) {
+                            System.out.println("Operação inválida, saldo insuficiente");
+                    } else {
+                        saldoInicial -= valorTransferido;
+                        System.out.printf("O seu saldo atualizado é de R$ %.2f%n", saldoInicial);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Obrigado por usar nosso banco");
+                    break;
+                default:
+                    System.out.println("Operação inválida.");
+                    break;
+            }
+        }
     }
 }
